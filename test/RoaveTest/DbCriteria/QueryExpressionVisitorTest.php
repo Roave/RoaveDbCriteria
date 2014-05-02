0cc9a2c34a6cf25766a9589acbbd62f8c7dbbd59
Aleksey Khudyakov <aleksey@xerkus.pro>
2014-05-03 08:35:14 +1100

    Add composite expression tests

--- test/RoaveTest/DbCriteria/QueryExpressionVisitorTest.php
+++ test/RoaveTest/DbCriteria/QueryExpressionVisitorTest.php
@@ -4,15 +4,49 @@ namespace RoaveTest\DbCriteria;
 use PHPUnit_Framework_TestCase;
 use Roave\DbCriteria\QueryExpressionVisitor;
 use Doctrine\Common\Collections\Criteria;
+use Zend\Db\Sql\Predicate\PredicateSet;
 
 class QueryExpressionVisitorTest extends PHPUnit_Framework_TestCase
 {
     public function testWhereClauseHasExpectedOperator()
     {
         $criteria = Criteria::create()
-            ->where(Criteria::expr()->eq('ID', 123));
+            ->where(Criteria::expr()->eq('foo', 123));
         $visitor   = new QueryExpressionVisitor();
-        $predicate     = $visitor->dispatch($criteria->getWhereExpression());
+        $predicate = $visitor->dispatch($criteria->getWhereExpression());
         $this->assertEquals($predicate::OP_EQ, $predicate->getOperator());
     }
+
+    public function testCompositeExpressionProducesProperPredicateSet()
+    {
+        $criteria = Criteria::create()
+            ->where(Criteria::expr()->eq('foo', 123));
+        $criteria->andWhere(Criteria::expr()->eq('bar', '321'));
+
+        $visitor   = new QueryExpressionVisitor();
+
+        $predicateSet = $visitor->dispatch($criteria->getWhereExpression());
+        $this->assertInstanceOf('Zend\Db\Sql\Predicate\PredicateSet', $predicateSet);
+        foreach ($predicateSet as $predicateChild) {
+            $this->assertEquals(PredicateSet::OP_AND, $predicateChild[0]);
+            $this->assertInstanceOf('Zend\Db\Sql\Predicate\PredicateInterface', $predicateChild[1]);
+        }
+    }
+
+    public function testCompositeOrExpressionProducesProperPredicateSet()
+    {
+        $criteria = Criteria::create()
+            ->where(Criteria::expr()->eq('foo', 123));
+        $criteria->orWhere(Criteria::expr()->eq('bar', '321'));
+
+        $visitor   = new QueryExpressionVisitor();
+
+        $predicateSet = $visitor->dispatch($criteria->getWhereExpression());
+        $this->assertInstanceOf('Zend\Db\Sql\Predicate\PredicateSet', $predicateSet);
+        foreach ($predicateSet as $predicateChild) {
+            $this->assertEquals(PredicateSet::OP_OR, $predicateChild[0]);
+            $this->assertInstanceOf('Zend\Db\Sql\Predicate\PredicateInterface', $predicateChild[1]);
+        }
+    }
+
 }
