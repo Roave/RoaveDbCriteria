4d7eb0e06053c947984e35179543227e35a85d6f
Marco Pivetta <ocramius@gmail.com>
2014-05-03 07:29:06 +0200

    Test case that shows that NOT IN() predicates are not produced correctly

--- test/RoaveTest/DbCriteria/QueryExpressionVisitorTest.php
+++ test/RoaveTest/DbCriteria/QueryExpressionVisitorTest.php
@@ -71,4 +71,12 @@ class QueryExpressionVisitorTest extends PHPUnit_Framework_TestCase
         $this->assertSame('true', $predicate->getExpression());
     }
 
+    public function testProducesNotInPredicate()
+    {
+        $visitor   = new QueryExpressionVisitor();
+        $criteria  = Criteria::create()->where(Criteria::expr()->notIn('foo', array('bar', 'baz')));
+        $predicate = $visitor->dispatch($criteria->getWhereExpression());
+
+        $this->assertInstanceOf('Zend\Db\Sql\Predicate\Expression', $predicate);
+    }
 }
